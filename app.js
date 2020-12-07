dee352b410e4b3e117513fb4654725f5e858ab99
Paradox001-pro <60643966+Paradox001-pro@users.noreply.github.com>
2020-12-07 16:05:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -34,14 +34,14 @@ require('./config/passport')(passport);
 
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://paradoxCodes:IC@nDoIt2@cluster0.nrckv.mongodb.net/nhubtestApp?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://paradoxCodes:IC@nDoIt2@cluster0.nrckv.mongodb.net/nhubtestApp?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
 
